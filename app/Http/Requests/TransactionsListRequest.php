1c2e29587d2f1fc767b132ced112e567b7c73975
stdOWL <osman.ozturk2@hotmail.com>
2024-11-14 02:41:15 +0000

    Update validation rules in TransactionsListRequest to include specific allowed values for status, operation, paymentMethod, errorCode, and filterField

--- app/Http/Requests/TransactionsListRequest.php
+++ app/Http/Requests/TransactionsListRequest.php
@@ -16,13 +16,13 @@ class TransactionsListRequest extends FormRequest
         return [
             'fromDate' => ['required', 'date', 'date_format:Y-m-d'],
             'toDate' => ['required', 'date', 'date_format:Y-m-d'],
-            'status' => ['nullable', 'string'],
-            'operation' => ['nullable', 'string'],
+            'status' => ['nullable', 'string', 'in:APPROVED,WAITING,DECLINED,ERROR'],
+            'operation' => ['nullable', 'string', 'in:DIRECT,REFUND,3D,3DAUTH,STORED'],
             'merchantId' => ['nullable', 'integer'],
             'acquirerId' => ['nullable', 'integer'],
-            'paymentMethod' => ['nullable', 'string'],
-            'errorCode' => ['nullable', 'string'],
-            'filterField' => ['nullable', 'string'],
+            'paymentMethod' => ['nullable', 'string', 'in:CREDITCARD,CUP,IDEAL,GIROPAY,MISTERCASH,STORED,PAYTOCARD,CEPBANK,CITADEL'],
+            'errorCode' => ['nullable', 'string', 'in:Do not honor,Invalid transaction,Invalid card,Not sufficient funds,Incorrect PIN,Invalid country association,Currency not allowed','3-D Secure Transport Error,Transaction not permitted to cardholder'],
+            'filterField' => ['nullable', 'string', 'in:Transaction UUID,Customer Email,Reference No', 'Custom Data', 'Card PAN'],
             'filterValue' => ['nullable', 'string'],
             'page' => ['nullable', 'integer', 'min:1'],
         ];
