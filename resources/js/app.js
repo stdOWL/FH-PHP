899be18764f28e7bb131fc0048a0bc026b881c51
stdOWL <osman.ozturk2@hotmail.com>
2024-11-14 02:59:00 +0000

    FE - Enhance client and transaction info display by implementing jsonFormatter for better formatting in bootstrap tables

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,16 @@
 $( document ).ready(function() {
+    const jsonFormatter = (value, row, prefix = '') => {
+        if (!value || typeof value !== 'object')
+            return value;
 
+        const keys = Object.keys(value);
+        return keys.map(key => {
+            if(typeof value[key] === 'object' ){
+                return `<p>${key}: ${jsonFormatter(value[key], row, `${prefix}${key}.`)}</p>`
+            }
+            return `<p>${key}: ${value[key]}</p>`
+        }).join('');
+    };
     const fetchClientInfo = (transactionId) => {
         const token = localStorage.getItem("token");
         $.ajax({
@@ -13,7 +24,57 @@ $( document ).ready(function() {
             success: function(data){
                 if(data.customerInfo){
                     const customerInfo = data.customerInfo;
-                    $("#clientInfoDetail").html(JSON.stringify(customerInfo));
+                    $("#clientInfoDetail").bootstrapTable({
+                        columns: [
+
+                        {
+                            field: 'id',
+                            title: 'Id',
+                            formatter: jsonFormatter
+                        },
+                        {
+                            field: 'billingAddress1',
+                            title: 'Billing Address',
+                            formatter: jsonFormatter
+                        },
+                        {
+                            field: 'billingCity',
+                            title: 'Billing City',
+                            formatter: jsonFormatter
+                        },
+                        {
+                            field: 'billingCompany',
+                            title: 'Billing Company',
+                            formatter: jsonFormatter
+                        },
+                        {
+                            field: 'billingFirstName',
+                            title: 'Billing First Name',
+                            formatter: jsonFormatter
+                        },
+                        {
+                            field: 'billingLastName',
+                            title: 'Billing Last Name',
+                            formatter: jsonFormatter
+                        },
+                        {
+                            field: 'created_at',
+                            title: 'Created At',
+                            formatter: jsonFormatter
+                        },
+                        {
+                            field: 'email',
+                            title: 'Email',
+                            formatter: jsonFormatter
+                        },
+                        {
+                            field: 'updated_at',
+                            title: 'Updated At',
+                            formatter: jsonFormatter
+                        }
+                    ],
+                        data: [customerInfo]
+                    })
                 }
             },
             error: function(errMsg) {
@@ -32,10 +93,87 @@ $( document ).ready(function() {
                 "Authorization": token
             },
             dataType: "json",
-            success: function(data){
-                if(data.current_page){
-                    const txInfo = data?.data;
-                    $("#txInfoDetail").html(JSON.stringify(txInfo));
+            success: function(response){
+                if(response.transaction){
+                    const merchantInfo = response.transaction.merchant;
+                    // 'referenceNo', 'merchantId', 'status', 'channel', 'customData', 'chainId', 'type', 'agentInfoId', 'operation', 'updated_at', 'created_at', 'id', 'fxTransactionId', 'acquirerTransactionId', 'code', 'message', 'transactionId', 'agent
+                    $("#txInfoDetail").bootstrapTable({
+                        columns: [{
+                            field: 'referenceNo',
+                            title: 'Reference No',
+                            formatter: jsonFormatter
+                        }, {
+                            field: 'merchantId',
+                            title: 'Merchant Id',
+                            formatter: jsonFormatter
+                        }, {
+                            field: 'status',
+                            title: 'Status',
+                            formatter: jsonFormatter
+                        }, {
+                            field: 'channel',
+                            title: 'Channel',
+                            formatter: jsonFormatter
+                        }, {
+                            field: 'customData',
+                            title: 'Custom Data',
+                            formatter: jsonFormatter
+                        }, {
+                            field: 'chainId',
+                            title: 'Chain Id',
+                            formatter: jsonFormatter
+                        }, {
+                            field: 'type',
+                            title: 'Type',
+                            formatter: jsonFormatter
+                        }, {
+                            field: 'agentInfoId',
+                            title: 'Agent Info Id',
+                            formatter: jsonFormatter
+                        }, {
+                            field: 'operation',
+                            title: 'Operation',
+                            formatter: jsonFormatter
+                        }, {
+                            field: 'updated_at',
+                            title: 'Updated At',
+                            formatter: jsonFormatter
+                        }, {
+                            field: 'created_at',
+                            title: 'Created At',
+                            formatter: jsonFormatter
+                        }, {
+                            field: 'id',
+                            title: 'Id',
+                            formatter: jsonFormatter
+                        }, {
+                            field: 'fxTransactionId',
+                            title: 'FX Transaction Id',
+                            formatter: jsonFormatter
+                        }, {
+                            field: 'acquirerTransactionId',
+                            title: 'Acquirer Transaction Id',
+                            formatter: jsonFormatter
+                        }, {
+                            field: 'code',
+                            title: 'Code',
+                            formatter: jsonFormatter
+                        }, {
+                            field: 'message',
+                            title: 'Message',
+                            formatter: jsonFormatter
+                        }, {
+                            field: 'transactionId',
+                            title: 'Transaction Id',
+                            formatter: jsonFormatter
+                        }, {
+                            field: 'agent',
+                            title: 'Agent',
+                            formatter: jsonFormatter
+                        }],
+                        data: [merchantInfo]
+                    });
+
                 }
             },
             error: function(errMsg) {
@@ -55,31 +193,31 @@ $( document ).ready(function() {
                 "Authorization": token
             },
             dataType: "json",
-            success: function(data){
-                if(data.status === "APPROVED"){
-                    const info = data?.data;
+            success: function(response){
+                if(response.data){
+                    const info = response?.data;
                     $('#txQuery-table').bootstrapTable({
                         columns: [{
                             field: 'customerInfo',
                             title: 'Customer Info',
-                            formatter: (value, row) => JSON.stringify(value)
+                            formatter: jsonFormatter
                         }, {
                             field: 'merchant',
                             title: 'Merchant Info',
-                            formatter: (value, row) => JSON.stringify(value)
+                            formatter: jsonFormatter
 
                         }, {
                             field: 'fx',
                             title: 'FX Info',
-                            formatter: (value, row) => JSON.stringify(value)
+                            formatter: jsonFormatter
 
                         }, {
                             field: 'transaction',
                             title: 'TX Info',
-                            formatter: (value, row) => JSON.stringify(value)
+                            formatter: jsonFormatter
 
                         }],
-                        data: info.data
+                        data: info
                     })
                 }
             },
@@ -101,18 +239,21 @@ $( document ).ready(function() {
             },
             dataType: "json",
             success: function(data){
-                if(data.status === "OK"){
-                    const info = data?.data;
+                if(data.response){
+                    const info = data?.response;
                     $('#txReport-table').bootstrapTable({
                         columns: [{
                             field: 'currency',
-                            title: 'currency'
+                            title: 'currency',
+                            formatter: jsonFormatter
                         }, {
                             field: 'count',
-                            title: 'count'
+                            title: 'count',
+                            formatter: jsonFormatter
                         }, {
                             field: 'total',
-                            title: 'total'
+                            title: 'total',
+                            formatter: jsonFormatter
                         }],
                         data: info
                     })
